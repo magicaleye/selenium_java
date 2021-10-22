bb27b6d2917127a1104251ad1290744f9210e532
DQhieu <hieudoquang1@gmail.com>
2021-10-22 14:12:14 +0700

    Initial commit

---
+++ src/test/java/BrowserTest.java
@@ -0,0 +1,10 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BrowserTest {
+  public static void main(String[] args) {
+      WebDriver driver = new FirefoxDriver();
+      driver.get("https://opensource-demo.orangehrmlive.com/");
+  }
+}
