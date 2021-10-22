5d3733f90806ab7e2fc812c9384007676a773a01
devilsprings <hieudoquang1@gmail.com>
2021-10-22 18:13:17 +0700

    Code formatting

--- src/test/java/hrms/test/HRMSLoginTest.java
+++ src/test/java/hrms/test/HRMSLoginTest.java
@@ -5,19 +5,20 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import pageObject.HrmsLoginPageObject;
 
 public class HRMSLoginTest {
-   private static WebDriver driver = null;
+  private static WebDriver driver = null;
+
   public static void main(String[] args) throws InterruptedException {
     userLogin();
   }
 
   public static void userLogin() throws InterruptedException {
-      driver = new ChromeDriver();
-      HrmsLoginPageObject loginPageObject = new HrmsLoginPageObject(driver);
+    driver = new ChromeDriver();
+    HrmsLoginPageObject loginPageObject = new HrmsLoginPageObject(driver);
 
-      driver.get("http://192.168.67.35/");
-      loginPageObject.enterLoginCredentials("ptqanh","123456a@");
-      loginPageObject.clickLoginButton();
-      Thread.sleep(6000);
-      driver.close();
+    driver.get("http://192.168.67.35/");
+    loginPageObject.enterLoginCredentials("ptqanh", "123456a@");
+    loginPageObject.clickLoginButton();
+    Thread.sleep(6000);
+    driver.close();
   }
 }
