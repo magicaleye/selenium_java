858df210a0b61b9b4ff107b482f679397943e574
DQhieu <hieudoquang1@gmail.com>
2021-10-22 15:23:58 +0700

    Add Valid login test case

---
+++ src/test/java/PageObject/LoginPage.java
@@ -0,0 +1,9 @@
+package PageObject;
+
+public class LoginPage {
+  public static class Screen {
+    public static final String txtBoxUsername = "//input[@id='normal_login_username']";
+    public static final String txtBoxPassword = "//input[@id='normal_login_password']";
+    public static final String btnLogin = "//button[@type='submit']";
+  }
+}
